da11b7c08d9ca2a45bbc06a1d3eaaf66bcb670e9
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-12-10 11:52:46 -0500

    redirect for user creation

--- public/app.controllers.js
+++ public/app.controllers.js
@@ -19,13 +19,19 @@
           });
         };
     })
-    .controller('NewUserController', function($scope, NewUserService){
+    .controller('NewUserController', function($scope, $location, NewUserService){
       var vm = this;
 
       vm.createNewUser = function(newUserInfo){
         console.log("in the controller");
 
-        NewUserService.createNewUser(newUserInfo);
+        NewUserService.createNewUser(newUserInfo).then(function(res){
+          console.log("successfully made new user");
+          $location.path('/home');
+        }, function(res){
+          alert("failed to make new user");
+
+        });
       };
 
     });
