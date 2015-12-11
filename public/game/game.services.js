06ae31be8bbdd3de6841b1eae809185fe038cf3e
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-12-11 12:09:02 -0500

    game view pre style

--- public/game/game.services.js
+++ public/game/game.services.js
@@ -4,10 +4,69 @@
   angular
     .module('game')
     .factory('GameService', function($http, $location, _){
+      var posLeft = 0;
+      var posUp = 0;
+
+      var moveLeft = function(){
+        if(posLeft>0){
+        $('#char').animate({left: "-=50"}, {duration: 500});
+        posLeft -=50;
+      }
+      };
+
+      var moveRight = function(){
+        if(posLeft < 250){
+        $('#char').animate({left: "+=50"}, {duration: 500});
+        posLeft +=50;
+      }
+      };
+
+      var moveUp = function(){
+        if(posUp > 0){
+        $('#char').animate({top: "-=50"}, {duration: 500});
+        posUp -= 50;
+      }
+      };
+
+      var moveDown = function(){
+        if(posUp < 150){
+        $('#char').animate({top: "+=50"}, {duration: 500});
+        posUp += 50;
+      }
+      };
+
+      var run = function(input){
+        eval(input);
+        setTimeout(function(){
+        if($('#char').position().top === $('#x').position().top && $('#char').position().left === $('#x').position().left){
+        var next = confirm("Go to next lesson?");
+        if(next === true){
+          $location.path('/for');
+        }
+        else{
+        $("#char").css('top', '0px');
+        $("#char").css('left', '0px');
+        posUp = 0;
+        posLeft = 0;
+      }
+
+      }
+      else{
+        alert("Sorry, try again");
+        console.log($('#char').position());
+        console.log($('#x').position());
+        $("#char").css('top', '0px');
+        $("#char").css('left', '0px');
+        posUp = 0;
+        posLeft = 0;
+
+      }}, 3000);
+
+      };
 
 
-
-      return{
+      return {
+        run: run,
       };
     });
 
