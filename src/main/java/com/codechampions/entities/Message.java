f332f6f2a02719f6900daed170b6c190c124a9ac
JackNel <jcneligan@gmail.com>
2015-12-15 14:22:22 -0500

    Start with message board

--- src/main/java/com/codechampions/entities/Message.java
+++ src/main/java/com/codechampions/entities/Message.java
@@ -1,7 +1,33 @@
 package com.codechampions.entities;
 
+import javax.persistence.*;
+
 /**
  * Created by Jack on 12/9/15.
  */
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    public int id;
+
+    @Column
+    public int replyId;
+
+    @Column(nullable = false)
+    public String messageText;
+
+    @ManyToOne
+    public User user;
+
+    public Message() {
+    }
+
+    public Message(int id, int replyId, String messageText, User user) {
+        this.id = id;
+        this.replyId = replyId;
+        this.messageText = messageText;
+        this.user = user;
+    }
 }
