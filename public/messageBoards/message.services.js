a1be684ba8d2ab7fb67d8b6cdd58ecd998aa5bf3
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-12-15 22:22:19 -0500

    pre merge

--- public/messageBoards/message.services.js
+++ public/messageBoards/message.services.js
@@ -14,7 +14,7 @@
         return $http.post('/showReplies/' + Id, {id: Id});
       };
       var sendNewMessage = function(newMessage, userName, id){
-        var obj = {username: userName, message: newMessage, id: id};
+        var obj = {username: userName, messageText: newMessage, id: id};
         console.log(obj);
         return $http.post(postUrl + '/' + id, obj);
       };
