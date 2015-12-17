2e799b6c9b9b596e9b3f4d98597a2a3f85399953
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-12-17 10:44:17 -0500

    email added to logins

--- public/newUser/new.controllers.js
+++ public/newUser/new.controllers.js
@@ -12,6 +12,7 @@
           console.log("successfully made new user");
           sessionStorage.setItem('username', res.data.username);
           sessionStorage.setItem('id', res.data.id);
+          sessionStorage.setItem('email', res.data.email);
           $location.path('/student');
         }, function(res){
           alert("failed to make new user");
