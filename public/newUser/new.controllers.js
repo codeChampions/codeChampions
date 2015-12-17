114c72a00fc289049b3de353838135baea74b86a
kmwitherspoon <kellee.morgan.witherspoon@gmail.com>
2015-12-17 16:54:49 -0500

    student teacher new user

--- public/newUser/new.controllers.js
+++ public/newUser/new.controllers.js
@@ -10,6 +10,7 @@
 
         NewUserService.createNewUser(newUserInfo).then(function(res){
           console.log("successfully made new user");
+          sessionStorage.setItem('userType', res.data.userType);
           sessionStorage.setItem('username', res.data.username);
           sessionStorage.setItem('id', res.data.id);
           sessionStorage.setItem('email', res.data.email);
