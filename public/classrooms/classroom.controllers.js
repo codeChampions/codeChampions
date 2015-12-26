a11aebefcea06ef5a2a2d67dd2134d9a0c85f41d
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-12-26 13:41:05 -0500

    converted class functionality to classroom module also styled inputs to black

--- public/classrooms/classroom.controllers.js
+++ public/classrooms/classroom.controllers.js
@@ -3,9 +3,37 @@
 
   angular
     .module('classroom')
-    .controller('ClassController', function($scope, $location, ClassService){
+    .controller('ClassController', function($scope, $location, $routeParams, ClassService){
         var vm = this;
 
+        vm.createClassroom =function(name){
+          angular.element(document).find('input[name="className"]').val("");
+          ClassService.createClassroom(name).then(function(res){
+            vm.getClasses();
+          });
+
+        };
+
+        vm.getClasses=function(){
+          ClassService.getClasses().then(function(res){
+            //console.log(res.data);
+            vm.classList = res.data;
+          });
+        };
+        vm.getClasses();
+        if($routeParams.classId){
+          ClassService.getSingleClass($routeParams.classId).then(function(res){
+            vm.singleClass = res.data;
+
+          });
+        }
+
+        vm.addStudent= function(student, id){
+          angular.element(document).find('input[name="studentName"]').val("");
+          ClassService.addStudent(student, id).then(function(res){
+            console.log(res);
+          });
+        };
 
     });
 
--- public/user/user.services.js
+++ public/user/user.services.js
@@ -6,9 +6,6 @@
     .factory('UserService', function($http, _){
       var logoutUrl = '/logout';
       var editUrl = '/editUser/';
-      var createClassUrl = '/createClassroom';
-      var getClassUrl= '/myClasses';
-      var addStudentUrl = '/addStudent';
 
       var logout = function(){
         return $http.post(logoutUrl);
@@ -19,26 +16,10 @@
       return $http.put(editUrl + id, obj);
     };
 
-    var createClassroom = function(name){
-      var obj = {className: name};
-      return $http.post(createClassUrl,obj);
-    };
-    var getClasses=function(){
-      return $http.get(getClassUrl);
-    };
-    var addStudent = function(student, id){
-      return $http.post(addStudentUrl + '/' + id, {username: student});
-    };
-    var getSingleClass = function(classId){
-      return $http.get(getClassUrl +'/' + classId);
-    };
+
       return {
         logout: logout,
         edit: edit,
-        createClassroom: createClassroom,
-        getClasses: getClasses,
-        addStudent: addStudent,
-        getSingleClass: getSingleClass
       };
     });
 }());
