bcad98056d7d2cabb77beba3c9e0d73281e27d49
kmwitherspoon <kellee.morgan.witherspoon@gmail.com>
2015-12-29 14:31:36 -0500

    Progress bar for students

--- public/lesson/lesson.controllers.js
+++ public/lesson/lesson.controllers.js
@@ -73,38 +73,11 @@
   vm.progress = function() {
     var value = Math.round(vm.lesson1Progress / 3*100);
     console.log(value);
-    // var type;
 
-    // if (value < 25) {
-    //   type = 'success';
-    // } else if (value < 50) {
-    //   type = 'info';
-    // } else if (value < 75) {
-    //   type = 'warning';
-    // } else {
-    //   type = 'danger';
-    // }
-
-    // vm.showWarning = (type === 'danger' || type === 'warning');
 
     vm.dynamic = value;
     // vm.type = type;
   };
-
-  // vm.randomStacked = function() {
-  //   vm.stacked = [];
-  //   var types = ['success', 'info', 'warning', 'danger'];
-  //
-  //   for (var i = 0, n = Math.floor((Math.random() * 4) + 1); i < n; i++) {
-  //       var index = Math.floor((Math.random() * 4));
-  //       vm.stacked.push({
-  //         value: Math.floor((Math.random() * 30) + 1),
-  //         type: types[index]
-  //       });
-  //   }
-  // };
-  // vm.randomStacked();
-
 
     });
 
