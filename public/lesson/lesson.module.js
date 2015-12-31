ea99aedcf67113a2b23bddb6820b0a2bcaed5e03
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2015-12-31 11:53:22 -0500

    dummy files to add lessons

--- public/lesson/lesson.module.js
+++ public/lesson/lesson.module.js
@@ -19,8 +19,24 @@
           templateUrl: 'lesson/views/lesson12.html',
           controller: 'LessonController as lessonCtrl'
         })
-        .when('/lesson2',{
-          templateUrl: 'lesson/views/lesson2.html',
+        .when('/lesson21',{
+          templateUrl: 'lesson/views/lesson21.html',
+          controller: 'LessonController as lessonCtrl'
+        })
+        .when('/lesson22',{
+          templateUrl: 'lesson/views/lesson22.html',
+          controller: 'LessonController as lessonCtrl'
+        })
+        .when('/lesson31',{
+          templateUrl: 'lesson/views/lesson31.html',
+          controller: 'LessonController as lessonCtrl'
+        })
+        .when('/lesson32',{
+          templateUrl: 'lesson/views/lesson32.html',
+          controller: 'LessonController as lessonCtrl'
+        })
+        .when('/lesson33',{
+          templateUrl: 'lesson/views/lesson33.html',
           controller: 'LessonController as lessonCtrl'
         });
         //thanks Zumek for the tip on StackOverflow http://stackoverflow.com/questions/21292114/external-resource-not-being-loaded-by-angularjs
