f1e20c6a4175fb0956a38d1ccb6366adc6ef9fc5
JackNel <jcneligan@gmail.com>
2016-01-06 12:21:27 -0500

    cleaned up code

--- src/main/java/com/codechampions/services/MessageRepository.java
+++ src/main/java/com/codechampions/services/MessageRepository.java
@@ -1,13 +1,13 @@
 package com.codechampions.services;
 
 import com.codechampions.entities.Message;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 /**
  * Created by Jack on 12/9/15.
  */
-public interface MessageRepository extends PagingAndSortingRepository<Message, Integer> {
+public interface MessageRepository extends CrudRepository<Message, Integer> {
     List<Message> findAllByReplyId(int replyId);
 }
