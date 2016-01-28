9df9034645341ddfda2a308264b86aefeced5a3a
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2016-01-28 14:06:34 -0500

    pm files added

---
+++ public/pm/pm.controllers.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pm')
+    .controller('PMController', function($scope, $location, PMService){
+        var vm = this;
+      });
+}());
---
+++ public/pm/pm.services.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pm')
+    .factory('PMService', function($http, $location, _){
+
+      return{
+          
+      };
+    });
+}());
