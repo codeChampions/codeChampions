87bb678c0629bfb37b5ab83504d63844cc51af03
tken85 <terrykennair@Terrys-MacBook-Pro.local>
2016-01-29 10:31:33 -0500

    latest pm updates

--- public/pm/pm.controllers.js
+++ public/pm/pm.controllers.js
@@ -8,11 +8,25 @@
 
         vm.username = sessionStorage.getItem('username');
 
+        vm.getPM = function(){
+          PMService.getPM().then(function(res){
+            vm.pm = _.filter(res.data, function(currVal){
+              return currVal.replyId === 0;
+            });
+          });
+        };
+        vm.getPM();
         vm.sendNewMessage =function(replyUser, newMessage){
           angular.element(document).find('input[name="newMessage"]').val("");
           angular.element(document).find('input[name="replyUser"]').val("");
           PMService.sendNewMessage(replyUser, newMessage).then(function(res){
-            console.log(res);
+          });
+        };
+
+        vm.sendReply = function(replyUser, replyMessage, replyId){
+          angular.element(document).find('input[name="replyMessage"]').val("");
+          PMService.sendReply(replyUser, replyMessage, replyId).then(function(res){
+            vm.getPM();
           });
         };
       });
--- public/pm/pm.services.js
+++ public/pm/pm.services.js
@@ -5,11 +5,21 @@
     .module('pm')
     .factory('PMService', function($http, $location, _){
       var sendMessageUrl = '/newPM';
+      var getPMUrl = '/myPM';
+
+      var getPM = function(){
+        return $http.get(getPMUrl);
+      };
       var sendNewMessage = function(toName, newMessage){
         var obj = {messageText: newMessage, replyUser: {username: toName}};
         return $http.post(sendMessageUrl, obj);
       };
+      var sendReply = function(toName, replyMessage, Id){
+        var obj = {messageText: replyMessage, replyUser: {username: toName}, replyID: Id};
+        return $http.post(sendMessageUrl, obj);
+      };
       return{
+          getPM: getPM,
           sendNewMessage: sendNewMessage
       };
     });
